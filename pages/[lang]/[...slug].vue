2e141d3b3cdf56be1471c05f2fafe40000c8d6a6
gijs <gijs.roge@intracto.com>
2022-01-18 00:04:32 +0100

    Add working async component setup + async data inside async component

--- pages/[lang]/[...slug].vue
+++ pages/[lang]/[...slug].vue
@@ -1,13 +1,24 @@
 <script lang="ts" setup>
+import { defineAsyncComponent } from "vue";
+
 const { data } = await useStoryblokPage();
+
+const fetchComponent = (component) => {
+  const components = import.meta.glob("../../components/*.vue");
+  return defineAsyncComponent(components[`../../components/${component}.vue`]);
+};
 </script>
 
 <template>
   <div>
     <h1>{{ data.story.name }}</h1>
+
     <ul>
       <li v-for="block in data.story.content.body">
-        <component :is="block.component" :blok="block"></component>
+        <component
+          :is="fetchComponent(block.component)"
+          :blok="block"
+        ></component>
       </li>
     </ul>
   </div>
